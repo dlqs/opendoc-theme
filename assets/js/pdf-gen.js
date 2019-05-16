5d77a8663aac6ea37e41a69487619d6c201a77e3
dlqs <donaldleeqs@gmail.com>
2019-05-16 17:05:18 +0800

    update script to generate pdf for every document
     - A document is defined as the combination of files a top level folder (from
       the main directory).
     - Since jekyll build will generate a .html file for every .md file, we
       simply have to concatenate the .html files per folder.
     - This is done by combining just the DOM nodes that hold the content
       then printing the whole thing out as a .pdf.
     - There is also a new html template to concatenate all the DOM nodes on.

--- assets/js/pdf-gen.js
+++ assets/js/pdf-gen.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
 var pdf = require('html-pdf')
+var glob = require('glob')
+var path = require('path')
+var jsdom = require('jsdom')
 var sitePath = __dirname + '/../..'
 var html = fs.readFileSync(sitePath + '/export.html', 'utf8');
 var options = {
@@ -21,7 +24,46 @@ var options = {
 console.log('Creating pdf.....')
 console.log('base: ', options.base)
 
+// creating export of all html files concatenated together
 pdf.create(html, options).toFile('./_site/export.pdf', function (err, res) {
     if (err) return console.log(err)
     console.log('Pdf created at: ', res)
+})
+
+// creating exports of individual documents
+var printIgnore = ['assets', 'files', 'iframes', 'images']
+var docFolders = fs.readdirSync(sitePath).filter(function(filePath) {
+    return fs.statSync(path.join(sitePath, filePath)).isDirectory() && 
+        !printIgnore.includes(filePath)
+})
+
+
+docFolders.forEach(function(folder) {
+    // find all the folders containing html files
+    var folderPath = path.join(sitePath, folder)
+    var htmlFilePaths = glob.sync(path.join(folderPath, '*.html'))
+    if (htmlFilePaths.length === 0) {
+        return
+    }
+
+    exportHtmlFile = jsdom.JSDOM.fromFile(sitePath + '/../_layouts/docprint.html')
+        .then(function(exportDom) {
+            var promises = []
+            htmlFilePaths.forEach(function(filePath) {
+                promises.push(
+                    jsdom.JSDOM.fromFile(filePath).then(function(dom) {
+                        // Concat all the main-content divs
+                        exportDom.window.document.body.appendChild(
+                            dom.window.document.getElementById('main-content')
+                    )
+                }))
+            })
+            // Wait for all concats to be completed
+            Promise.all(promises).then(function() {
+                pdf.create(exportDom.serialize(), options).toFile(path.join(folderPath, 'export.pdf'), function (err, res) {
+                    if (err) return console.log(err)
+                    console.log('Pdf created at: ', res.filename)
+                })
+            })
+        })
 })
