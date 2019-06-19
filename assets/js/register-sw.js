5d0aef9cf623239e324c1cdbdcfe9f5c52232ef1
dlqs <donaldleeqs@gmail.com>
2019-06-19 17:39:43 +0800

    debug

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -1,7 +1,23 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (registration) {
-        console.log("ServiceWorker registration successful with scope: ", registration.scope);
-    }).catch(function (err) {
-        console.log("ServiceWorker registration failed: ", err);
-    });
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (registration) {
+            console.log("ServiceWorker registration successful with scope: ", registration.scope)
+        }).catch(function (err) {
+            console.log("ServiceWorker registration failed: ", err)
+        })
+
+        window.addEventListener('appinstalled', function(e) {
+            var msgChannel = new MessageChannel()
+            msgChannel.port1.onmessage = function(event) {
+                if (event.data === 'done') {
+                    // sw is done caching
+                    document.getElementById('sw-loading-icon').style.display = 'none'
+                }
+            }
+
+            // Tell sw to start caching process, may take several seconds
+            navigator.serviceWorker.controller.postMessage('appinstalled', [msgChannel.port2])
+            document.getElementById('sw-loading-icon').style.display = 'block'
+        })
+    })
 }
