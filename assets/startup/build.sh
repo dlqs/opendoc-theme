0c99ffa9d1f8d8a1a1a9435e6846ca4bf1dd9a5f
dlqs <donaldleeqs@gmail.com>
2019-07-09 10:55:07 +0800

    haha

--- assets/startup/build.sh
+++ assets/startup/build.sh
@@ -8,4 +8,5 @@ npm i glob jsdom js-yaml p-all
 if [[ (-z "${PDF_GEN_API_SERVER}") || (-z "${PDF_GEN_API_KEY}") ]]; then
   npm i html-pdf
 fi
+node _site/assets/startup/pdf-gen.js
 echo 'End script'
